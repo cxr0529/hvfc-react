9999919f2bc1e04ed42bb8c63cb9cb6d4175360e
cxr0529 <cxr0529@gmail.com>
2024-10-26 15:46:45 -0400

    moved files

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -9,8 +9,7 @@ function Navbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar fixed="top" />
-        <Navbar.Brand href="#home"><img className="d-inline-block align-top" src={'../src/images/logo.jpg'} alt="Logo"/>{' '}Harrisville VFC</Navbar.Brand>
+        <Navbar.Brand href="#home"><img className="d-inline-block align-top" src="images/logo.jpg" alt="Logo"/>{' '}Harrisville VFC</Navbar.Brand>
         <Navbar.Toggle aria-controls="hvfc-navbar" />
         <Navbar.Collapse id="hvfc-navbar">
           <Nav className="me-auto">
@@ -18,23 +17,23 @@ function Navbar() {
             <NavDropdown title="About Us" id="about-us-dropdown">
               <NavDropdown.Item href="#contact">Contact Us</NavDropdown.Item>
               <NavDropdown.Item href="#history">History</NavDropdown.Item>
-              <NavDropdown.Item href="../src/docs/HVFC-BY-LAWS-2023.pdf" target="_blank">By-Laws</NavDropdown.Item>
+              <NavDropdown.Item href="docs/HVFCBYLAWS2023.pdf" target="_blank" rel="noreferrer">ByLaws</NavDropdown.Item>
               <NavDropdown.Item href="#construction">Call History</NavDropdown.Item>
               <NavDropdown.Item href="#apparatus">Apparatus</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Member Info" id="member-dropdown">
               <NavDropdown.Item href="#contact">Current Members</NavDropdown.Item>
               <NavDropdown.Item href="#history">Lifetime Members</NavDropdown.Item>
-              <NavDropdown.Item href="../src/docs/HVFCMembershipApplication.pdf" target="_blank">Membership Application</NavDropdown.Item>
+              <NavDropdown.Item href="docs/HVFCMembershipApplication.pdf" target="_blank" rel="noreferrer">Membership Application</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Hall Rental" id="hall-rental-dropdown">
               <NavDropdown.Item href="#calendar">Availability Calendar</NavDropdown.Item>
               <NavDropdown.Item href="#rentalGallery">Photo Gallery</NavDropdown.Item>
-              <NavDropdown.Item href="../src/docs/HallRentalFAQ.pdf" target="_blank">FAQ</NavDropdown.Item>
-              <NavDropdown.Item href="../src/docs/docs/HVFC-RentalAgreement.pdf" target="_blank">Hall Rental Application</NavDropdown.Item>
+              <NavDropdown.Item href="docs/HallRentalFAQ.pdf" target="_blank" rel="noreferrer">FAQ</NavDropdown.Item>
+              <NavDropdown.Item href="docs/HVFC-RentalAgreement.pdf" target="_blank" rel="noreferrer">Hall Rental Application</NavDropdown.Item>
             <NavDropdown title="Useful Links" id="useful-links-dropdown">
               <NavDropdown.Item href="#links">Fire Departments</NavDropdown.Item>
-              <NavDropdown.Item href="../src/docs/AddressSignsOrderforms.pdf" target="_blank">Address Sign Order Form</NavDropdown.Item>
+              <NavDropdown.Item href="docs/AddressSignsOrderforms.pdf" target="_blank" rel="noreferrer">Address Sign Order Form</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Photo Gallery" id="photo-gallery-dropdown">
               <NavDropdown.Item href="#trucks">Trucks</NavDropdown.Item>
@@ -44,9 +43,9 @@ function Navbar() {
               <NavDropdown.Item href="#apparatus">Accidents</NavDropdown.Item>
               <NavDropdown.Item href="#misc">Misc.</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="https://www.facebook.com/hvfc1938" target="_blank"><img src={'../src/images/Facebook.png'} alt="Facebook" /></Nav.Link>
-            <Nav.Link href="https://www.instagram.com/hvfc1938" target="_blank"><img src={'../src/images/Instagram.png'} alt="Instagram" /></Nav.Link>
-            <Nav.Link href="https://www.ready.gov/kids/games" target="_blank">Kids Zone</Nav.Link>
+            <Nav.Link href="https://www.facebook.com/hvfc1938" target="_blank" rel="noreferrer"><img src="images/facebook.png" alt="Facebook"/></Nav.Link>
+            <Nav.Link href="https://www.instagram.com/hvfc1938" target="_blank" rel="noreferrer"><img src="images/instagram.png" alt="Instagram"/></Nav.Link>
+            <Nav.Link href="https://www.ready.gov/kids/games" target="_blank" rel="noreferrer">Kid's Zone</Nav.Link>
             <Nav.Link href="tel: 7247354646">Give us a call</Nav.Link>
           </Nav>
         </Navbar.Collapse>
